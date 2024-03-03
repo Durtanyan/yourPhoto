bfdbaf27b654f0ebc8a21c62191c688d5568cfba
Durtanyan <lukins.ya@yandex.ru>
2024-03-03 18:08:51 +0300

    изменено:      js/addPhotosToThePage.js адаптировал размеры фотографий под размеры блоков

--- js/addPhotosToThePage.js
+++ js/addPhotosToThePage.js
@@ -1,23 +1,26 @@
-export function addPhotosToThePage (photosForThePage) {
-	for (let i = 0; i < photosForThePage.length; i++) {
-		$("#root-block")
-			.append(`<div class='background-picture' id=background-picture-${i}></div>`);
-		let blockWidth = document.documentElement.clientWidth;
-		let blockHeight = document.documentElement.clientHeight;
-		let imageUrl;
-		if (blockWidth > blockHeight) {
-			imageUrl = photosForThePage[i];
-			console.log(imageUrl);
-			$(`#background-picture-${i}`).css("background-image", `url(${imageUrl})`);
-			$(`#background-picture-${i}`).css("height", `${blockHeight}px`);
-			$(`#background-picture-${i}`).css("width", `${blockWidth / 2}px`);
-			let marginLeft = (blockWidth - (blockWidth / 2)) / 2;
-			$(`.background-picture`).css("margin-left", marginLeft);
-		} else {
-			imageUrl = photosForThePage[i];
-			$(`#background-picture-${i}`).css("background-image", `url(${imageUrl})`);
-			$(`#background-picture-${i}`).css("width", `${blockWidth}px`);
-			$(`#background-picture-${i}`).css("height", `${blockHeight}px`);
-		}
-	}
+export function addPhotosToThePage(photosForThePage) {
+  for (let i = 0; i < photosForThePage.length; i++) {
+    let imageUrl = photosForThePage[i];
+    $("#root-block")
+      .append(`<div class='background-picture' id=background-picture-${i}>
+			<img src=${imageUrl} id=img-${i}>
+			</div>`);
+    let blockWidth = document.documentElement.clientWidth;
+    let blockHeight = document.documentElement.clientHeight;
+    if (blockWidth > blockHeight) {
+      $("img").css("maxWidth", "100%");
+      $("img").css("maxHeight", "100%");
+      $(`.background-picture`).css("display", "flex");
+      $(`.background-picture`).css("justify-content", "center");
+      $(`.background-picture`).css("align-items", "center");
+      $(`#background-picture-${i}`).css("height", `${blockHeight}px`);
+      $(`#background-picture-${i}`).css("width", `${blockWidth / 2}px`);
+      let marginLeft = (blockWidth - blockWidth / 2) / 2;
+      $(`.background-picture`).css("margin-left", marginLeft);
+    } else {
+			$("img").css("maxWidth", "100%");
+			$("img").css("maxHeight", "100%");
+      $(`.background-picture`).css("text-align", "center");
+    }
+  }
 }
