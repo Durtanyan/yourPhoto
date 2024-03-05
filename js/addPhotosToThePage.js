f4433b89cfa1ce9710999afd619c119d25d7f2c1
Durtanyan <lukins.ya@yandex.ru>
2024-03-05 20:18:42 +0300

    Добавил страницу заглушку для Ню...

--- js/addPhotosToThePage.js
+++ js/addPhotosToThePage.js
@@ -18,10 +18,37 @@ export function addPhotosToThePage(photosForThePage) {
       let marginLeft = (blockWidth - blockWidth / 2) / 2;
       $(`.background-picture`).css("margin-left", marginLeft);
     } else {
-			$("img").css("maxWidth", "97%");
-			$("img").css("maxHeight", "97%");
-			$("img").css("padding-top", "5px")
+      $("img").css("maxWidth", "97%");
+      $("img").css("maxHeight", "97%");
+      $("img").css("padding-top", "5px");
       $(`.background-picture`).css("text-align", "center");
     }
   }
+}
+
+export function theStubPage() {
+  $("#root-block").append(`<div class="the-stub-page"></div>`);
+  $(".the-stub-page").append(
+    `<div>
+      <button class="button-the-stub-page yes">Мне уже есть 18 лет.</button>
+      <button class="button-the-stub-page no">Мне еще нет 18 лет.</button>
+    </div>`
+  );
+  let blockWidth = document.documentElement.clientWidth;
+  let blockHeight = document.documentElement.clientHeight;
+  $(".the-stub-page").css({
+    height: `${blockHeight}px`,
+    width: `${blockWidth}px`,
+  });
+  $(`.the-stub-page`).css("text-align", "center");
+  $(`.button-the-stub-page`).css({
+    width: "50%",
+    height: "40%",
+    "background-color": "red",
+		"font-size": "20px",
+		"color": "white",
+		"border": "5px solid white",
+		"margin": "10% 10% 0% 10%",
+		"border-radius": "5%"
+  });
 }
